588b08b1e8b19671a091558976a519ed0aad5c15
pemulaco <154732483+pemulaco@users.noreply.github.com>
2024-03-05 20:21:56 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function abc() {
+    document.getElementById('active').style.display="none";
+    document.getElementById('unactive').style.display="block";
+    document.getElementById('body').style.background="whitesmoke";
+    document.getElementById('box').style.background="#fff";
+    document.getElementById('h4').style.color="steelblue";
+
+}
+function bca() {
+    document.getElementById('unactive').style.display="none";
+    document.getElementById('active').style.display="block";
+    document.getElementById('body').style.background="#000";
+    document.getElementById('box').style.background="#000";
+    document.getElementById('h4').style.color="#3fa";
+
+}
